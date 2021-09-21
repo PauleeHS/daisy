4332e33b0ae4b25167a7e49fda73f5abe0988f6a
neogi-code <neogi-code@users.noreply.github.com>
2021-09-21 16:19:58 +0900

    db check

--- common/db.php
+++ common/db.php
@@ -2,7 +2,7 @@
   session_start();
   
   // db주고, 계정, 비밀번호, db 이름
-  $db = new mysqli("211.183.3.101:3333","root","test123","daisy");
+  $db = new mysqli("211.183.3.101:3333","root","test123","daisy",3333);
   $db->set_charset("utf8"); //utf 8 인코딩
 
   function mq($sql){
